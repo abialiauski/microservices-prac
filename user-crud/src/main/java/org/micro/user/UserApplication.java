ce78c2b858a5cfecb79843b5d8f5e5cd3df5539d
Ivan Karavai <user@users-MacBook-Pro.local>
2022-07-22 11:17:04 +0300

    Stable user-crud pre-compose

--- user-crud/src/main/java/org/micro/user/UserApplication.java
+++ user-crud/src/main/java/org/micro/user/UserApplication.java
@@ -2,10 +2,10 @@ package org.micro.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+//import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients
+//@EnableFeignClients
 public class UserApplication {
 
     public static void main(String[] args) {
